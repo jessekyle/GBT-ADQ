b0620de82802070a27625ece6c9e4e88b1741164
Jesse <contact.jkyle@gmail.com>
2017-06-28 16:05:04 -0700

    updating

--- Test1A.py
+++ Test1A.py
@@ -19,3 +19,37 @@ elif answer == "ant_motion: Guiding":
 else:
     print("Not Slewing or Guiding")
 
+
+# In[7]:
+
+import subprocess
+newline = None
+
+Test1 = subprocess.run(["echo", "'select ant_motion from status;'",
+                        "|", "mysql", "-h", "gbtdata.gbt.nrao.edu", "-u",
+                        "gbtstatus", "--password=w3bquery", "-E",
+                        "gbt_status"], 
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE)
+print("{Test1.stdout}".format(Test1=Test1))
+
+
+# In[42]:
+
+import subprocess
+from subprocess import Popen, PIPE
+
+Test1 = subprocess.Popen(["echo", "'hello'"], 
+                       stdout=PIPE)
+
+out, err = Test1.communicate()
+print(out)
+
+
+
+
+
+# In[ ]:
+
+
+
